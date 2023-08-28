486bdff408128b5903903617c001010c11a9e505
Heavenston <malolegendrelemaire@@gmail.com>
2023-08-28 08:26:26 +0200

    Cobweb and Cobweb Node

---
+++ cobweb/src/node.rs
@@ -0,0 +1,73 @@
+use std::{net::SocketAddr, sync::Arc};
+
+use stridul::{StridulSocket, StridulStrategy, StridulSocketDriver};
+
+use tokio::net::{UdpSocket, ToSocketAddrs};
+
+#[derive(Debug, Clone)]
+pub struct CobwebStridulStrategy;
+impl StridulStrategy for CobwebStridulStrategy {
+    type Socket = UdpSocket;
+    type PeersAddr = SocketAddr;
+
+    const BASE_WINDOW_SIZE: u32 = stridul::StridulUDPStrategy::BASE_WINDOW_SIZE;
+    const BASE_RTO: std::time::Duration = stridul::StridulUDPStrategy::BASE_RTO;
+    const PACKET_MAX_SIZE: u32 = stridul::StridulUDPStrategy::PACKET_MAX_SIZE;
+    const BUFFER_MAX_SIZE: usize = stridul::StridulUDPStrategy::BUFFER_MAX_SIZE;
+}
+
+// TODO: Make it generic-based to avoid runtime errors on build
+#[derive(Default)]
+pub struct NodeBuilder {
+    socket: Option<Arc<StridulSocket<CobwebStridulStrategy>>>,
+    socket_driver: Option<StridulSocketDriver<CobwebStridulStrategy>>,
+}
+
+impl NodeBuilder {
+    pub fn new() -> Self {
+        NodeBuilder::default()
+    }
+
+    pub async fn bind(
+        &mut self, addr: impl ToSocketAddrs
+    ) -> anyhow::Result<&mut Self> {
+        let udp_socket = UdpSocket::bind(addr).await?;
+        self.with_udp_socket(udp_socket);
+        Ok(self)
+    }
+
+    pub fn with_stridul_socket(
+        &mut self,
+        socket: Arc<StridulSocket<CobwebStridulStrategy>>,
+        socket_driver: StridulSocketDriver<CobwebStridulStrategy>,
+    ) -> &mut Self {
+        self.socket = Some(socket);
+        self.socket_driver = Some(socket_driver);
+        self
+    }
+
+    pub fn with_udp_socket(
+        &mut self, udp_socket: UdpSocket,
+    ) -> &mut Self {
+        let (socket, driver) = StridulSocket::new(udp_socket);
+        self.socket = Some(socket);
+        self.socket_driver = Some(driver);
+        self
+    }
+
+    pub fn build(&mut self) -> Node {
+        Node {
+            socket: self.socket.take().expect("Unfinished builder")
+        }
+    }
+}
+
+pub struct Node {
+    socket: Arc<StridulSocket<CobwebStridulStrategy>>,
+}
+
+impl Node {
+    pub fn builder() -> NodeBuilder {
+        NodeBuilder::default()
+    }
+}
