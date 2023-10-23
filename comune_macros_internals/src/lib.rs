766293fc0a3eb09578b30aeed467b12db00502d2
Heavenston <malolegendrelemaire@@gmail.com>
2023-10-23 14:11:53 +0200

    Use internal proc macro in parser

--- comune_macros_internals/src/lib.rs
+++ comune_macros_internals/src/lib.rs
@@ -1,9 +1,12 @@
 #![feature(if_let_guard)]
 
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput, DataEnum, ItemEnum};
+
+const UGLY_IDEN_PREFIX: &'static str = "__COMUNE_MACROS_INTERNALS";
 
 #[proc_macro_derive(TryAs)]
 pub fn derive_try_as(tt: TokenStream) -> TokenStream {
@@ -13,10 +16,31 @@ pub fn derive_try_as(tt: TokenStream) -> TokenStream {
 
     let (im_gen, ty_gen, wh_cla) = i.generics.split_for_impl();
 
-    let mut ex = quote!{};
+    let mut ex = quote! {};
 
-    for variant in i.variants {
-        let var_name = variant.ident;
+    let included_trait_name = syn::Ident::new(
+        &format!("{UGLY_IDEN_PREFIX}_included_{}", enum_name),
+        Span::call_site()
+    );
+
+    ex = quote! {
+        #ex
+        trait #included_trait_name {
+            const INDEX: usize;
+        }
+    };
+
+    ex = quote!{
+        #ex
+        impl #im_gen #enum_name #ty_gen #wh_cla {
+            pub fn index_of<T: #included_trait_name>() -> usize {
+                T::INDEX
+            }
+        }
+    };
+
+    for (i, variant) in i.variants.iter().enumerate() {
+        let var_name = &variant.ident;
         let var_type = match &variant.fields {
             syn::Fields::Unnamed(u) if let Some(f) = u.unnamed.first() => {
                 &f.ty
@@ -29,8 +53,15 @@ pub fn derive_try_as(tt: TokenStream) -> TokenStream {
             }
         };
 
+        let str_i = format!("{i}");
+        let lit_i = syn::LitInt::new(&str_i, Span::call_site());
+
         ex = quote! {
             #ex
+            impl #im_gen #included_trait_name for #var_type #wh_cla {
+                const INDEX: usize = #lit_i;
+            }
+
             impl #im_gen crate::try_as::TryAsRef<#var_type> for #enum_name #ty_gen #wh_cla {
                 fn try_as_ref(&self) -> Option<&#var_type> {
                     match self {
