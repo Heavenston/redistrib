6a52a7eab56bf07f0959432d97bbe2e8062751ee
Heavenston <malolegendrelemaire@@gmail.com>
2023-10-27 16:35:26 +0200

    TryAsRef auto impl from AsRef

--- comune/src/try_as.rs
+++ comune/src/try_as.rs
@@ -3,6 +3,22 @@ pub trait TryAsRef<T> {
     fn try_as_ref(&self) -> Option<&T>;
 }
 
+impl<T, Y> TryAsRef<T> for Y
+    where Y: AsRef<T>
+{
+    fn try_as_ref(&self) -> Option<&T> {
+        Some(self.as_ref())
+    }
+}
+
 pub trait TryAsMut<T> {
     fn try_as_mut(&mut self) -> Option<&mut T>;
+}
+
+impl<T, Y> TryAsMut<T> for Y
+    where Y: AsMut<T>
+{
+    fn try_as_mut(&mut self) -> Option<&mut T> {
+        Some(self.as_mut())
+    }
 }
