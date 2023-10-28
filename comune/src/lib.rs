ace4a19b3c70ad8bf3d6b4bdc623931ae4c0fe22
Heavenston <malolegendrelemaire@@gmail.com>
2023-10-28 19:20:46 +0200

    Removed feature flag of impl in return type in traits

--- comune/src/lib.rs
+++ comune/src/lib.rs
@@ -2,7 +2,6 @@
 
 #![allow(incomplete_features)]
 #![feature(adt_const_params)]
-#![feature(return_position_impl_trait_in_trait)]
 #![feature(never_type)]
 #![feature(generic_const_exprs)]
 #![feature(assert_matches)]
