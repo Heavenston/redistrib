15dc06c9a704604189737c26eb12f1425c4871ca
Heavenston <malolegendrelemaire@@gmail.com>
2023-11-04 17:04:39 +0100

    Removed index from try as

--- comune_macros_internals/src/lib.rs
+++ comune_macros_internals/src/lib.rs
@@ -1,12 +1,9 @@
 #![feature(if_let_guard)]
 
 use proc_macro::TokenStream;
-use proc_macro2::Span;
 
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput, DataEnum, ItemEnum};
-
-const UGLY_IDEN_PREFIX: &'static str = "__COMUNE_MACROS_INTERNALS";
+use syn::{parse_macro_input, ItemEnum};
 
 #[proc_macro_derive(TryAs)]
 pub fn derive_try_as(tt: TokenStream) -> TokenStream {
@@ -18,28 +15,7 @@ pub fn derive_try_as(tt: TokenStream) -> TokenStream {
 
     let mut ex = quote! {};
 
-    let included_trait_name = syn::Ident::new(
-        &format!("{UGLY_IDEN_PREFIX}_included_{}", enum_name),
-        Span::call_site()
-    );
-
-    ex = quote! {
-        #ex
-        trait #included_trait_name {
-            const INDEX: usize;
-        }
-    };
-
-    ex = quote!{
-        #ex
-        impl #im_gen #enum_name #ty_gen #wh_cla {
-            pub fn index_of<T: #included_trait_name>() -> usize {
-                T::INDEX
-            }
-        }
-    };
-
-    for (i, variant) in i.variants.iter().enumerate() {
+    for variant in i.variants.iter() {
         let var_name = &variant.ident;
         let var_type = match &variant.fields {
             syn::Fields::Unnamed(u) if let Some(f) = u.unnamed.first() => {
@@ -53,15 +29,8 @@ pub fn derive_try_as(tt: TokenStream) -> TokenStream {
             }
         };
 
-        let str_i = format!("{i}");
-        let lit_i = syn::LitInt::new(&str_i, Span::call_site());
-
         ex = quote! {
             #ex
-            impl #im_gen #included_trait_name for #var_type #wh_cla {
-                const INDEX: usize = #lit_i;
-            }
-
             impl #im_gen crate::try_as::TryAsRef<#var_type> for #enum_name #ty_gen #wh_cla {
                 fn try_as_ref(&self) -> Option<&#var_type> {
                     match self {
